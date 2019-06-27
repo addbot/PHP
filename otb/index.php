bee75ecaafbfdd25014f212b69c30c06829dd75b
sasori <marvin.dev.ph@gmail.com>
2019-06-27 22:29:11 +0800

    #0001 Initial Code commit for otb c/o Abi Hart

---
+++ otb/index.php
@@ -0,0 +1,13 @@
+<?php
+/***************************************************************************
+ * Filename  : index.php
+ * Purpose   : This file serves as the entry point.
+ * it instantiates objects and should execute the command parameter in cli.
+ * This code is part of the request from otb company. c/o Abi Hart
+ * Author(s) : marvin
+ * Date      : Date(June 27 2019)
+ ****************************************************************************/
+
+
+
+?>
---
+++ otb/otb.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+ * Filename  : Jobs.php
+ * Purpose   : This is the main class file which contains the main functions
+ * of all the job related processes in the system.
+ * This code is part of the request from otb company. c/0 Abi Hart
+ * Author(s) : marvin
+ * Date      : Date(June 27 2019)
+ ****************************************************************************/
+
+class Jobs {
+
+}
+
+
+
+?>
