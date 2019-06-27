89025e1fb4239bad4432034dbedf0dc838c599e6
sasori <marvin.dev.ph@gmail.com>
2019-06-28 07:01:29 +0800

    #0002 added some initial functions. Note: to friends who are following this old github account of mine. Please ignore these things that I am committing in my repository lol

--- otb/index.php
+++ otb/index.php
@@ -8,6 +8,76 @@
  * Date      : Date(June 27 2019)
  ****************************************************************************/
 
+function autoLoadEmuPhp($class) {
+	require $class . '.php';
+}
 
+$className = '';
+$command = '';
+$param = '';
+// p($argv);
+if (!empty($argv)) {
+	$className = @$argv[1];
+	if (isset($argv[2])) {
+		$command = $argv[2];
+	}
+	if (isset($argv[3])) {
+		$param = $argv[3];
+	} else {
+		$param = null;
+	}
+	spl_autoload_register(function ($className) {
+		require $className . '.php';
+	});
+} else {
+	displayUsage();
+}
+
+function p($var) {
+	echo '<pre>',print_r($var),'</pre>';exit;
+}
+
+function s($var) {
+	// echo '<pre>',print_r($var),'</pre>';
+	echo $var;exit;
+}
+
+function v($var) {
+	echo '<pre>',print_r(var_dump($var)),'</pre>';exit;
+}
+
+
+if (!empty($className) && class_exists($className)) {
+	$classObj = new $className;
+	if (!empty($command) && method_exists($classObj,$command)) {
+		if (!empty($param)) {
+			$classObj->$command($param);
+		} else {
+			$classObj->$command($param);			
+		}		
+	} else {
+		displayUsage();
+	}
+} else {
+	displayUsage();
+}
+
+function displayUsage() {
+
+$usage ="\n\n##################################################
+#
+# Jobs Processing Usage
+#
+#
+##################################################\n
+Usage: php index.php otb <command>\n
+Note:\n
+
+Command Options:\n
+getjobs - will return string or array if an empty parameter was passed\n\n\n";
+
+print $usage;
+    
+}
 
 ?>
--- otb/otb.php
+++ otb/otb.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- * Filename  : Jobs.php
+ * Filename  : otb.php
  * Purpose   : This is the main class file which contains the main functions
  * of all the job related processes in the system.
  * This code is part of the request from otb company. c/0 Abi Hart
@@ -8,7 +8,44 @@
  * Date      : Date(June 27 2019)
  ****************************************************************************/
 
-class Jobs {
+class Otb {
+
+	/**
+	 * constructor to execute commands for 
+     * jobs objects
+	 * @return type
+	 */
+	public function __construct() {
+		$this->getjobs();
+	}
+
+    public function getJobs($job = null): string {
+
+        $sequence = 'a';
+        if (!is_null($job)) {
+            if (is_array($job)) {
+                if (count($job) == 1) {
+                    $jobStr = implode(',',$job);
+                    $sequence = $jobStr;
+                } else if (count($job) > 1) {
+                    echo 'here';exit;
+                    if (in_array('c', $job)) {
+
+                    } else {
+                        $sequence = 'abc';
+                    }
+
+                }
+                
+            } else if (is_string($job)) {
+                $jobArr = explode(",",$job);
+            }
+
+        } 
+        s($sequence);
+        return $sequence;
+
+    }
 
 }
 
